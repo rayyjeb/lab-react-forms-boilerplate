1bdbe85889d22570797e4d3e1005a28fb37c9e70
rayyjeb <rajabshoukathis@gmail.com>
2024-03-04 12:31:07 +0530

    forms

---
+++ forms/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Forms from './Components/Forms'
+
+function App() {
+
+  return (
+    <div>
+      <Forms/>
+    </div>
+  )
+}
+
+export default App
